984b370460649c5310d673745f217273edbcc093
Manuel Selch <manuel@selch.de>
2024-06-06 02:18:03 +0200

    import dependencies manager

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
         .iOS(.v16), .macOS(.v13), .macCatalyst(.v16)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "OfflineSync",
             targets: ["OfflineSync"]),
@@ -17,17 +16,15 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.0")),
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", .upToNextMajor(from: "0.15.3")),
-        .package(url: "https://github.com/ManuelSelch/Redux.git", .upToNextMajor(from: "1.1.3"))
+        .package(url: "https://github.com/ManuelSelch/Dependencies.git", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "OfflineSync",
             dependencies: [
                 .product(name: "Moya", package: "Moya"),
                 .product(name: "SQLite", package: "SQLite.swift"),
-                .product(name: "Redux", package: "Redux")
+                .product(name: "Dependencies", package: "Dependencies")
             ],
             path: "Sources"
         ),
